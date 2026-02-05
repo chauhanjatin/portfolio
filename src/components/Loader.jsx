6b13d7f3da5835fe5750cb047cd44eb1a6719eaa
chauhanjatin <chauhanjatin.25@gmail.com>
2026-02-05 11:39:56 +0530

    chnage

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,7 +7,7 @@ export default function Loader() {
       <div className="after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[300px] md:after:h-[1000px] after:bg-[linear-gradient(180deg,rgba(244,244,244,0)_-1%,#F4F4F4_100%)] z-10 w-full absolute top-0 h-[100vh] md:h-dvh flex items-center">
         <img src={herobg} alt="" className="w-full h-full opacity-35" />
       </div>
-      <span class="loader-45"> </span>      
+      <span className="loader-45"> </span>      
     </div>
   );
 }
